3164db6e3b78347daa0ece342bd67849ee62a84c
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2024-03-20 04:40:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,45 @@ query = "What's the interest rate on Ford's long term debt?"
 found_docs = qdrant.similarity_search(query)
 print(found_docs[0].page_content)
 
+### Formatting
+
+
+import textwrap
+
+def wrap_text_preserve_newlines(text, width=110):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+
+    return wrapped_text
+
+def process_llm_response(llm_response):
+    print(wrap_text_preserve_newlines(llm_response['result']))
+    print('\n\nSources:')
+    for source in llm_response["source_documents"]:
+        print(source.metadata['source'])
+
+### Adding mixtral this time
+
+["DEEPINFRA_API_TOKEN"] = "6rdEEEr8WLCOp1mMdS3MTgPgT9A9IRSw"
+
+from langchain.llms import DeepInfra
+
+llm = DeepInfra(model_id="mistralai/Mixtral-8x7B-Instruct-v0.1")
+llm.model_kwargs = {
+    "temperature": 0.7,
+    "repetition_penalty": 1.2,
+    "max_new_tokens": 2500,
+    "top_p": 0.9,
+}
+
+llm_response = qa_chain(query)
+process_llm_response(llm_response)
 
 # Managing chat history
 if 'chat_history' not in st.session_state:
@@ -50,6 +89,7 @@ if st.button("Send"):
     if user_input:
         st.session_state.chat_history.append({"message": user_input, "is_user": True})
         response = found_docs[0].page_content
-        st.session_state.chat_history.append({"message": response, "is_user": False})
+        st.session_state.chat_history.append({"message": process_llm_response(llm_response)
+, "is_user": False})
         # Clear the input box after sending the message
         st.experimental_rerun()
